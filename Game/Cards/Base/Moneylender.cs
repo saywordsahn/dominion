96054eceea3a91bacc9a949af1c2822d82d830bc
Benjamin Christensen <saywordsahn@gmail.com>
2018-12-11 11:59:45 +0900

    Implemented Moneylender

--- Game/Cards/Base/Moneylender.cs
+++ Game/Cards/Base/Moneylender.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Linq;
+using DominionWeb.Game.Common;
 
 namespace DominionWeb.Game.Cards.Base
 {
-    public class Moneylender : ICard, IAction
+    public class Moneylender : ICard, IAction, IActionRequester
     {
         public int Cost { get; } = 4;
 
@@ -12,7 +14,27 @@ namespace DominionWeb.Game.Cards.Base
 
         public void Resolve(Game game)
         {
-            throw new NotImplementedException();
+            var player = game.GetActivePlayer();
+
+            if (player.Hand.Contains(Card.Copper))
+            {
+                player.ActionRequest = new YesNoActionRequest(Card.Moneylender, "Would you like to trash a copper to gain $3?");
+                player.PlayStatus = PlayStatus.ActionRequestResponder;
+            }
+        }
+
+        public void ResponseReceived(Game game, ActionResponse response)
+        {
+            var player = game.GetActivePlayer();
+            player.PlayStatus = PlayStatus.ActionPhase;
+
+            if (response == ActionResponse.Yes)
+            {
+                var copper = player.Hand.Find(x => x == Card.Copper);
+                player.TrashFromHand(game.Supply, copper);
+                player.MoneyPlayed += 3;
+            }
+
         }
     }
 }
