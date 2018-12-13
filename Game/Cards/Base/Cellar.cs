cab853e5eb349902b92ed7b88eb79d401e0d8982
Benjamin Christensen <saywordsahn@gmail.com>
2018-12-13 22:38:25 +0900

    Implemented Cellar

--- Game/Cards/Base/Cellar.cs
+++ Game/Cards/Base/Cellar.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using DominionWeb.Game.Common;
 
 namespace DominionWeb.Game.Cards.Base
 {
-    public class Cellar : ICard, IAction
+    public class Cellar : ICard, IAction, ISelectCardsResponseRequired
     {
         public int Cost { get; } = 2;
 
@@ -13,7 +15,39 @@ namespace DominionWeb.Game.Cards.Base
 
         public void Resolve(Game game)
         {
-            throw new NotImplementedException();
+            var player = game.GetActivePlayer();
+            player.NumberOfActions++;
+
+            if (player.Hand.Count > 0)
+            {
+                player.ActionRequest = new SelectCardsActionRequest("Select any number of cards to discard, +1 card for each.",
+                    Card.Cellar, player.Hand, player.Hand.Count);
+                player.PlayStatus = PlayStatus.ActionRequestResponder;
+            }
+            else
+            {
+                player.PlayStatus = PlayStatus.ActionPhase;
+            }
+        }
+
+        public void ResponseReceived(Game game, IEnumerable<Card> cards)
+        {
+            var player = game.GetActivePlayer();
+            var cardList = cards.ToList();
+
+            if (cardList.Count > 0)
+            {
+                foreach (var c in cardList)
+                {
+                    //TODO: add discardFromHandMethod on player
+                    player.Hand.Remove(c);
+                    player.DiscardPile.Add(c);
+                }
+                
+                player.Draw(cardList.Count); 
+            }
+
+            player.PlayStatus = PlayStatus.ActionPhase;
         }
     }
 }
---
+++ Tests/CardTests/BaseCardTests/CellarTests.cs
@@ -0,0 +1,68 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using DominionWeb.Game;
+using DominionWeb.Game.Common;
+using DominionWeb.Game.Player;
+using DominionWeb.Game.Supply;
+using Xunit;
+
+namespace DominionWeb.Tests.CardTests.BaseCardTests
+{
+    public class CellarTests
+    {
+//        [Theory]
+//        [InlineData(Card.Copper)]
+//        [InlineData(Card.Duchy)]
+//        public void cellar_card_is_gained(Card card)
+//        {
+//            const int supplyAmount = 10;
+//            var player = new Player(1, "b");
+//            var players = new List<IPlayer>() {player};
+//            var supply = new Supply(new List<Pile>(), new List<Pile>(), 
+//                new List<Pile>()
+//                {
+//                    new Pile(card, supplyAmount)
+//                });
+//            var vc = new VictoryCondition();
+//            var game = new Game.Game(0, players, supply, vc);
+//            
+//            player.GainToHand(Card.Artisan);
+//            player.PlayStatus = PlayStatus.ActionPhase;
+//            game.Submit(player.PlayerName, PlayerAction.Play, Card.Artisan);
+//            game.Submit(player.PlayerName, ActionRequestType.SelectCards, new List<Card> { card });
+//
+//            var actualSupplyCount = game.Supply.KingdomSupply.First().Cards.Count;
+//            
+//            Assert.Single(player.DiscardPile, x => x == card);
+//            Assert.Equal(actualSupplyCount, supplyAmount - 1);
+//        }
+        
+        [Fact]
+        public void cellar_discard_success()
+        {
+            var player = new Player(1, "b");
+            var players = new List<IPlayer>() {player};
+            var supply = new Supply(new List<Pile>(), new List<Pile>(), new List<Pile>());
+            var vc = new VictoryCondition();
+            var game = new Game.Game(0, players, supply, vc);
+            
+            player.GainToHand(Card.Cellar);
+            player.Hand.AddRange(Enumerable.Repeat(Card.Copper, 4));
+            player.Deck.AddRange(Enumerable.Repeat(Card.Silver, 5));
+            player.PlayStatus = PlayStatus.ActionPhase;
+            game.Submit(player.PlayerName, PlayerAction.Play, Card.Cellar);
+            game.Submit(player.PlayerName, ActionRequestType.SelectCards, Enumerable.Repeat(Card.Copper, 3));
+
+            var coppersInHand = player.Hand.Count(x => x == Card.Copper);
+            var silversInHand = player.Hand.Count(x => x == Card.Silver);
+            var discardCount = player.DiscardPile.Count();
+            
+            Assert.Equal(1, coppersInHand);
+            Assert.Equal(3, silversInHand);
+            Assert.Equal(3, discardCount);
+            Assert.Equal(1, player.PlayedCards.Count);
+            Assert.Equal(2, player.Deck.Count);
+        }
+    }
+}
